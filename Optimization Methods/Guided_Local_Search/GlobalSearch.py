01312d96b705d18c00291b22c6c5c812f6b0163d
Данил Муранов <danil5678444@gmail.com>
2023-01-22 14:24:19 +0300

    Fully developed local search/

--- Optimization Methods/Guided_Local_Search/GlobalSearch.py
+++ Optimization Methods/Guided_Local_Search/GlobalSearch.py
@@ -5,16 +5,15 @@ from LocalSearch import LocalSearch
 
 
 class GlobalSearch(LocalSearch):
-  def __init__(self, nsteps: int = 10000, eps: float = 1e-5, step: float = 1e-1, m: int = 100) -> None:
+  def __init__(self, nsteps: int = 10000, eps: float = 1e-5, step: float = 1e-1) -> None:
     super().__init__(nsteps, eps, step)
-    self.l = 0.1
-    self.m = m
-    self.p = np.zeros((self.m))
-    self.util = np.zeros((self.m))
+    self.l = 0.4
     self.c_criteria = 2.4
 
   def root(self, x: np.array, f: Callable[[np.array], np.array]) -> np.array:
     size = x.shape
+    self.p = np.zeros(size)
+    self.util = np.zeros(size)
     self.c = self.c_criteria * np.ones(size)
     self.I = np.zeros(size)
     self.s = self.s_by_f = [f(x)]
@@ -22,10 +21,25 @@ class GlobalSearch(LocalSearch):
     h = self._cost_function(self.s[-1])
     delta_f = 2 * self.eps
     while (k <= self.nsteps) & (delta_f >= self.eps):
-      self.s.append(self._root(self.s[-1], h))
-      for i in range(len(self.m)):
-        pass
-    return 
+      s_i, status = self._root(self.s[-1], h)
+      self.s_by_f.append(f(s_i))
+      self.s.append(s_i)
+      for i in range(len(size)):
+        self.util[i] = status[i] * self.c[i] / (1 + self.p[i])
+      indexes = np.argmax(self.util, -1)
+      self.p[indexes] += 1
+      k += 1
+    return self.s[np.argmin(self.s_by_f)[0]]
 
-  def _cost_function(self, cost: np.array) -> np.array:
+  def _cost_function(self, cost: float) -> np.array:
     return cost + self.l * (self.p @ self.I)
+
+
+def f(x: np.array) -> float:
+  return np.sum(np.power(np.cos(x) - 2 * np.ones(x.shape), 2))
+
+
+if __name__ == "__main__":
+  gls = GlobalSearch()
+  x = np.array([0., -10.])
+  root = gls.root(x, f)
--- Optimization Methods/Guided_Local_Search/LocalSearch.py
+++ Optimization Methods/Guided_Local_Search/LocalSearch.py
@@ -5,43 +5,73 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+# TO-DO:
+# 1. Нужно переписать условия выхода из цикла +
+# 2. Нужно понять как определять минимумы аргументов +
+# 3. Детальнее вникнуть в статью
+# 4. Написать GLS
+
+
 class LocalSearch:
-  def __init__(self, nsteps: int = 10000, eps: float = 1e-6, step: float = 0.1) -> None:
+  def __init__(self, nsteps: int = 1000, eps: float = 1e-6, step: float = .1) -> None:
     self.nsteps = nsteps
     self.eps = eps
     self.step = step
     self.history = []
 
   def _root(self, x: np.array, f: Callable[[np.array], np.array]) -> np.array:
+    self.status = True
     n = 0
-    delta_f = 2 * self.eps
-    while (np.max(np.abs(delta_f)) >= self.eps) & (self.nsteps > n):
-      ksi = self._get_ksi(x.shape[0])
-      n += 1
+    size = x.shape
+    ksi = self._get_ksi(size)
+    while True:
       f_i = f(x)
-      x_s = x + ksi
-      f_s = f(x_s)
-      for i in range(len(f_s)):
-        if f_s[i] < f_i[i]:
-          x[i] = x_s[i]
-      delta_f = f_s - f_i
-    return x, n
+      x_new = x.copy()
+      x_prev = x.copy()
+      for i in range(size[-1]):
+        if i >= 1:
+          x[i-1] = x_prev[i-1]
+        x[i] += self.step * ksi[i]
+        f_ii = f(x)
+        if f_ii - f_i >= 0:
+          ksi[i] = self._get_ksi(size)[i]
+        else:
+          x_new[i] = x[i]
+      x = x_new
+      if n >= self.nsteps:
+        self.status = False
+        break
+      if self._check_local_min(x, f):
+        break
+      n += 1
+    return x, self.status, n
+
+  def _check_local_min(self, x: np.array, f: Callable[[np.array], np.array]) -> bool:
+    status = True
+    x_p = x.copy()
+    x_m = x.copy()
+    for i in range(len(x)):
+      x_p[i] = x_p[i] + 100*self.eps
+      x_m[i] = x_m[i] - 100*self.eps
+      if (f(x) > f(x_m)) | (f(x) > f(x_p)):
+        status = False
+    return status
 
   def _get_ksi(self, size: int) -> np.array:
-    ksi = self.step * np.ones((1, size))
-    for i in range(size):
+    ksi = np.ones(size)
+    for i in range(size[-1]):
       dist = np.random.uniform(-1,1,1)
       if dist <= 0.:
         ksi[i] *= -1.
     return ksi
 
 
-def f(x: np.array) -> np.array:
-  return np.power(x, 2)
+def f(x: np.array) -> float:
+  return np.sum(np.power(x, 2), -1)
 
 
 if __name__ == '__main__':
   ls = LocalSearch()
-  x = np.array([20.])
-  root, n = ls.root(x, f)
-  print(root, n, '\n')
+  x = np.array([-20, 12])
+  root, status, n = ls._root(x, f)
+  print(root, status, n, '\n')
