bc5e6e6e80606d0e3725f453cf84280620925924
Данил Муранов <danil5678444@gmail.com>
2023-01-22 14:54:13 +0300

    The alpha version of guided local search

--- Optimization Methods/Guided_Local_Search/GlobalSearch.py
+++ Optimization Methods/Guided_Local_Search/GlobalSearch.py
@@ -5,33 +5,39 @@ from LocalSearch import LocalSearch
 
 
 class GlobalSearch(LocalSearch):
-  def __init__(self, nsteps: int = 10000, eps: float = 1e-5, step: float = 1e-1) -> None:
+  def __init__(self, nsteps: int = 10000, eps: float = 1e-5, step: float = 1e-1, m: int = 10, \
+    l: float = 0.4, c_criteria: float = 2.4) -> None:
     super().__init__(nsteps, eps, step)
-    self.l = 0.4
-    self.c_criteria = 2.4
+    self.l = l
+    self.m = m
+    self.c_criteria = c_criteria
 
   def root(self, x: np.array, f: Callable[[np.array], np.array]) -> np.array:
-    size = x.shape
-    self.p = np.zeros(size)
-    self.util = np.zeros(size)
-    self.c = self.c_criteria * np.ones(size)
-    self.I = np.zeros(size)
-    self.s = self.s_by_f = [f(x)]
+    self.p = np.zeros((self.m))
+    self.util = np.zeros((self.m))
+    self.c = self.c_criteria * np.ones((self.m))
+    self.I = np.zeros((self.m))
+    self.s = [x]
+    self.s_by_f = []
     k = 0
-    h = self._cost_function(self.s[-1])
-    delta_f = 2 * self.eps
-    while (k <= self.nsteps) & (delta_f >= self.eps):
-      s_i, status = self._root(self.s[-1], h)
-      self.s_by_f.append(f(s_i))
-      self.s.append(s_i)
-      for i in range(len(size)):
-        self.util[i] = status[i] * self.c[i] / (1 + self.p[i])
-      indexes = np.argmax(self.util, -1)
-      self.p[indexes] += 1
+    h = self._cost_function
+    while True:
+      x, status, n = self._root(self.s[-1], h)
+      self.s_by_f.append(f(self.s[-1]))
+      self.s.append(x)
+      self.I[k] = int(status)
+      for i in range(self.m):
+        self.util[i] = self.I[i] * self.c[i] / (1 + self.p[i])
+      indxs = np.argmax(self.util, -1)
+      self.p[indxs] += 1
       k += 1
-    return self.s[np.argmin(self.s_by_f)[0]]
+      if k >= self.m:
+        break
+    min_ind = np.argmin(self.s_by_f)
+    return self.s[min_ind]
 
-  def _cost_function(self, cost: float) -> np.array:
+  def _cost_function(self, x: np.array) -> np.array:
+    cost = f(x)
     return cost + self.l * (self.p @ self.I)
 
 
@@ -43,3 +49,4 @@ if __name__ == "__main__":
   gls = GlobalSearch()
   x = np.array([0., -10.])
   root = gls.root(x, f)
+  print(root)
