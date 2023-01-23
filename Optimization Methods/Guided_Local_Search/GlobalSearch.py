2778ff9a050f4d79712c2c746702319799e3e424
Данил Муранов <danil5678444@gmail.com>
2023-01-23 16:59:05 +0300

    Fixed errors in choosing the optimal solution

--- Optimization Methods/Guided_Local_Search/GlobalSearch.py
+++ Optimization Methods/Guided_Local_Search/GlobalSearch.py
@@ -5,7 +5,7 @@ from LocalSearch import LocalSearch
 
 
 class GlobalSearch(LocalSearch):
-  def __init__(self, nsteps: int = 10000, eps: float = 1e-5, step: float = 1e-1, m: int = 10, \
+  def __init__(self, nsteps: int = 10000, eps: float = 1e-5, step: float = 1e-1, m: int = 2, \
     l: float = 0.4, c_criteria: float = 2.4) -> None:
     super().__init__(nsteps, eps, step)
     self.l = l
@@ -18,13 +18,13 @@ class GlobalSearch(LocalSearch):
     self.c = self.c_criteria * np.ones((self.m))
     self.I = np.zeros((self.m))
     self.s = [x]
-    self.s_by_f = []
+    self.s_by_f = [f(self.s[-1])]
     k = 0
     h = self._cost_function
     while True:
       x, status, n = self._root(self.s[-1], h)
-      self.s_by_f.append(f(self.s[-1]))
       self.s.append(x)
+      self.s_by_f.append(f(self.s[-1]))
       self.I[k] = int(status)
       for i in range(self.m):
         self.util[i] = self.I[i] * self.c[i] / (1 + self.p[i])
@@ -33,8 +33,8 @@ class GlobalSearch(LocalSearch):
       k += 1
       if k >= self.m:
         break
-    min_ind = np.argmin(self.s_by_f)
-    return self.s[min_ind]
+    min_ind = np.where(self.s_by_f == np.min(self.s_by_f))
+    return self.s[min_ind[-1][-1]]
 
   def _cost_function(self, x: np.array) -> np.array:
     cost = f(x)
@@ -42,11 +42,11 @@ class GlobalSearch(LocalSearch):
 
 
 def f(x: np.array) -> float:
-  return np.sum(np.power(np.cos(x) - 2 * np.ones(x.shape), 2))
+  return np.sum(x)
 
 
 if __name__ == "__main__":
   gls = GlobalSearch()
-  x = np.array([0., -10.])
+  x = np.array([12., 14.])
   root = gls.root(x, f)
   print(root)
